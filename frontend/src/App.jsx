f92d12c1e7fc80d40ac290690bde695a34a38d95
My8921 <ahmadali43a5@gmail.com>
2025-04-01 12:29:40 +0500

    setup tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import React  from 'react';
 import './App.css'
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div  className=''>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
