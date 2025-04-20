d22476d7ab8c934eafe7a2b25bf04c0285f5c725
My8921 <ahmadali43a5@gmail.com>
2025-04-20 20:01:20 +0500

    setted additional paramters in useAuthStore

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,9 +1,24 @@
 import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.jsx";
 
 
-
-export const useAuthStore = ( (set)=>({
+export const useAuthStore = ((set)=>({
     authUser:null,
+    isSigningUp:false,
+    isLoggingIn:false,
+    isUpdatingProfile:false,
     isCheckingAuth:true,
+    checkAuth: async ()=>{
+        try {
+            const res = await axiosInstance.get('/auth/check-auth');
+            set({authUser:res.data});
+        } catch (error) {
+            set({authUser:null});
+        }
+        finally{
+            set({isCheckingAuth:false});
+        }
+    }
+
     
 }))
