21b45d3728966451118f6bea64b5a032071508b3
My8921 <ahmadali43a5@gmail.com>
2025-05-04 22:55:12 +0500

    setted up changes for live

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ import dotenv from 'dotenv';
 import {connectDB}  from './lib/db.js';
 import cookieParser from 'cookie-parser';
 
+import path from "path";
 
 dotenv.config();
 app.use(express.json());
@@ -15,8 +16,17 @@ app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true,
 }));
+
+
 app.use("/api/auth" , authRoutes);
 app.use("/api/messages" , messageRoute);
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
 const PORT = process.env.PORT || 5001;
 
 server.listen(PORT , () => {
